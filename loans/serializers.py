38888857b91e4c3a6714f792a5d4fff00481cfe6
Ziad Fahim <ziadmohamed44214@gmail.com>
2023-12-28 17:22:47 +0200

    first commit

--- loans/serializers.py
+++ loans/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Inquiry
 
-class CountrySerializer(serializers.ModelSerializer):
+class InquirySerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Inquiry

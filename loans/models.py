e65ce4af26e89003cbdead868a424befc7fa8909
Ziad Fahim <ziadmohamed44214@gmail.com>
2023-12-28 17:20:16 +0200

    first commit

---
+++ loans/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+
+# Create your models here.
+
+class Programs(models.Model):
+    name = models.CharField(max_length=100)
+    min_amount = models.PositiveIntegerField()
+    max_length = models.PositiveIntegerField()
+
+
+class Borrower(models.Model):
+    identification_number = models.CharField(max_length=20)
+    date_of_birth = models.DateField(auto_now=False, auto_now_add=False)
+
+
+class Inquiry(models.Model):
+    
+    STATUS = (
+        ('APPROVED', 'APPROVED'),
+        ('REJECTED', 'REJECTED')
+    )
+
+    program = models.ForeignKey(Programs, related_name="inquiry_program", on_delete=models.CASCADE)
+    borrower= models.ForeignKey(Borrower, related_name="inquiry_borrower", on_delete=models.CASCADE)
+    amount = models.PositiveIntegerField()
+    status = models.CharField(max_length=10, choices=STATUS)
+    rejection_reason = models.TextField(null=True, blank=True)
+
+class Untrusted(models.Model):
+    identification_number = models.CharField(max_length=20)
+
+
+
+
+
+
+"""
+Basheer Alsammarraie <basheer@audteye.com>
+	
+16:15 (27 minutes ago)
+	
+to me, Süheyb
+Task: Implement a Django API for loan inquiries.
+
+Utilize Django and Django REST framework for development and leverage Django Admin for managing app Models.
+
+Models:
+
+Program - Define minimum and maximum loan amounts and the applicable age range.
+
+Borrower - Include an ID number and Date of Birth (DOB).
+
+Inquiry - Record Program, Borrower, Loan Amount, Status (approved or rejected), and reason for rejection.
+
+Untrusted ID - Maintain a list of untrusted ID numbers.
+
+Previous Application - Save all previous loan applications.
+"""
---
+++ loans/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from .models import Inquiry
+
+class CountrySerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Inquiry
+        fields = "__all__"
+
+
+
+
+
+
+"""
+1. Receive input containing an ID number and loan amount.
+
+2. Verify if the loan amount falls within the specified range defined by the Program. If not, reject the inquiry.
+
+3. Determine the age of the Borrower based on the provided ID number (using the first 6 digits YYMMDD as the DOB). If the age is outside the applicable range, reject the inquiry.
+
+4. Implement a random logic to determine if the individual is a Sole Proprietor (e.g., generate a random boolean value). If the logic indicates they are, reject the inquiry.
+
+5. Check if the ID number is present in the Untrusted List. If it is, reject the inquiry.
+
+6. Save the inquiry as a PreviousApplication.
+
+7. Provide an approval or rejection status and the reason for rejection.
+"""
